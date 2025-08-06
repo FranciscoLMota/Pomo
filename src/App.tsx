5df9df5ab0048eb482dbb8fce235fffb679203a7
Francisco Mota <franciscolmota@outlook.com>
2025-08-06 17:02:38 -0400

    basic pomodoro processing

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import './App.css'
+import { Navbar } from './features/Navbar'
+import { Pomodoro } from './features/Pomodoro'
 
 function App() {
 
   return (
     <>
-      <p className='font-russo text-4xl'>POMO</p>
-      <p className="font-russo text-9xl border-12 border-whiteish p-3">
-        00:10
-      </p>
+      <div className="bg-onyx transition-all duration-500 font-russo h-screen" id="container">
+        <Navbar />
+        <Pomodoro />
+      
+      </div>
+
+
     </>
   )
 }
---
+++ src/features/Navbar.tsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import tomatoLogo from '../assets/tomato.svg'
+
+export function Navbar() {
+
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  return (
+    <>
+      <nav className="sticky top-0 z-55 backdrop-blur-sm transition-colors duration-500 my-0 ">
+        <div className="flex flex-wrap place-items-center overflow-hidden my-0">
+          <section className="relative">
+            <nav className="flex justify-between text-midnight dark:text-eggshell w-screen">
+              <div className="p-5 pt-0 flex w-full items-center">
+                <img src={tomatoLogo} className="h-25 w-25" alt="Vite logo" />
+                <ul className="md:flex px-4 mx-auto font-semibold font-heading space-x-12"></ul>
+                <ul className="xl:flex my-0  space-x-3 align-top pt-0">
+                   <p className='font-russo text-5xl text-whiteish align-top pt-0'>POMO</p>
+                </ul>
+              </div>
+
+
+            </nav>
+          </section>
+        </div>
+      </nav>
+    </>
+  );
+}
